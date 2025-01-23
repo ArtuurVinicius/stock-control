dfdf5938351119ad78b23c35e0c1f6d220665a9d
ArturVinicius <lartur671@gmail.com>
2025-01-23 12:34:06 -0300

    ft(add-new-component):adição do componente product-form

---
+++ src/app/services/categories/categories.service.ts
@@ -0,0 +1,26 @@
+import { HttpClient, HttpHeaders } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { CookieService } from 'ngx-cookie-service';
+import { Observable } from 'rxjs';
+import { GetCategoriesResponse } from 'src/app/models/interfaces/categories/responses/GetCategoriesResponse';
+import { environment } from 'src/environments/environment';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class CategoriesService {
+  private API_URL = environment.API_URL;
+  private JWT_TOKEN = this.cookie.get('USER_INFO');
+  private httpOption = {
+    headers: new HttpHeaders({
+      'Content-Type': 'application/json',
+      'Authorization': `Bearer ${this.JWT_TOKEN}`
+    })
+  }
+
+  constructor(private httpClient: HttpClient, private cookie: CookieService) { }
+
+  getAllCategories(): Observable<Array<GetCategoriesResponse>> {
+    return this.httpClient.get<Array<GetCategoriesResponse>>(`${this.API_URL}/categories`, this.httpOption);
+  }
+}
